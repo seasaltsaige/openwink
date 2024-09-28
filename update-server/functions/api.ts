8203e2969b61ac5d848fe0e26c04a327be72113a
Maxisthemoose <oriontothemax@gmail.com>
2024-09-28 15:26:37 -0700

    serverless api

---
+++ update-server/functions/api.ts
@@ -0,0 +1,63 @@
+import express, { NextFunction, Request, Response, Router } from "express";
+import serverless from "serverless-http";
+import fs from "fs";
+import path from "path";
+
+const app = express();
+
+
+const router = Router();
+
+const auth = async (req: Request, res: Response, next: NextFunction) => {
+  const authorization = req.headers.authorization;
+  if (authorization === undefined)
+    return res.status(403).json({ error: "Missing Authorization" });
+
+  const macDir = path.join(__dirname, "../files/mac.json");
+  if (!fs.existsSync(macDir)) return res.status(500).json({ error: "Internal Server Error" });
+
+  const data = fs.readFileSync(macDir, "ascii");
+  const jsonData = JSON.parse(data);
+
+  if (!jsonData || !jsonData.mac_addr) return res.status(500).json({ error: "Internal Server Error" });
+
+
+  const macs = jsonData.mac_addr as string[];
+  if (!macs) return res.status(500).json({ error: "Internal Server Error" });
+
+  if (!macs.includes(authorization))
+    return res.status(403).json({ error: "Invalid Authorization" });
+
+
+  next(null);
+}
+
+
+router.get("/", auth, async (req, res) => {
+  const pathToUpdateJson = path.join(__dirname, "../files/update.json");
+  console.log(pathToUpdateJson);
+  const pathToUpdateBin = path.join(__dirname, "../files/update.bin");
+
+  if (fs.existsSync(pathToUpdateJson)) {
+    const file = fs.readFileSync(pathToUpdateJson, "ascii");
+    const data = JSON.parse(file);
+
+    res.status(200).json(data);
+  } else
+    res.status(500).json({ error: "Update JSON file not found" });
+});
+
+router.get("/firmware", auth, async (req, res) => {
+  res.json({ message: "firmware bfhbfbhdhbsdhjbsdgvjbsdgjksd" });
+});
+
+
+app.use("/.netlify/functions/api/update", router);
+
+const handler = serverless(app);
+
+//@ts-ignore
+module.exports.handler = async (event, context) => {
+  const res = await handler(event, context);
+  return res;
+};
