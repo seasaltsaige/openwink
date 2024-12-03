9c23a46a1ffc06f42d6f087dac87f48c1a16eb00
seasaltsaige <oriontothemax@gmail.com>
2024-12-03 13:30:12 -0800

    init store classes

--- winkduino-app-3.0/hooks/useBLE.ts
+++ winkduino-app-3.0/hooks/useBLE.ts
@@ -2,4 +2,3 @@ import * as ExpoDevice from "expo-device";
 import base64 from "react-native-base64";
 import { PermissionsAndroid, Platform } from "react-native";
 import { BleManager, Device, ScanCallbackType, ScanMode, Subscription } from "react-native-ble-plx";
-
