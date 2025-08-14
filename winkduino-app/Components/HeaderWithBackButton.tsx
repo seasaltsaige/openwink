18a6bdca9a4987fc6623575991f0fdeb796509b3
seasaltsaige <oriontothemax@gmail.com>
2025-08-14 01:26:22 -0700

    create(header): Create HeaderWithBackButton.txt
    Component which handles the back action for each sub menu in the
    settings. Vastly over-repeated code.

---
+++ winkduino-app/Components/HeaderWithBackButton.tsx
@@ -0,0 +1,52 @@
+import { ActivityIndicator, Pressable, Text, View } from "react-native";
+import { useColorTheme } from "../hooks/useColorTheme";
+import { useNavigation } from "@react-navigation/native";
+import IonIcons from "@expo/vector-icons/Ionicons";
+import { useBLE } from "../hooks/useBLE";
+
+interface IHeaderWithBackButtonProps {
+  backText: string;
+  headerText: string;
+}
+
+export function HeaderWithBackButton({ backText, headerText }: IHeaderWithBackButtonProps) {
+  const { theme, colorTheme } = useColorTheme();
+  const { device, isConnecting, isScanning } = useBLE();
+  const navigation = useNavigation();
+
+  return (
+    <View style={theme.headerContainer}>
+      <Pressable
+        style={theme.backButtonContainer}
+        onPress={() => navigation.goBack()}
+      >
+        {
+          ({ pressed }) => (
+            <>
+              <IonIcons style={theme.backButtonContainerIcon} name="chevron-back-outline" color={pressed ? colorTheme.buttonColor : colorTheme.headerTextColor} size={23} />
+
+              <Text style={pressed ? theme.backButtonContainerTextPressed : theme.backButtonContainerText}>
+                {backText}
+              </Text>
+              {
+                device ? (
+                  <IonIcons style={theme.backButtonContainerIcon} name="wifi-outline" color="#367024" size={21} />
+                ) : (
+                  (isConnecting || isScanning) ?
+                    <ActivityIndicator style={theme.backButtonContainerIcon} color={colorTheme.buttonColor} />
+                    : (
+                      <IonIcons style={theme.backButtonContainerIcon} name="cloud-offline-outline" color="#b3b3b3" size={23} />
+                    )
+                )
+              }
+            </>
+          )
+        }
+      </Pressable>
+
+      <Text style={theme.subSettingHeaderText}>
+        {headerText}
+      </Text>
+    </View>
+  )
+}
