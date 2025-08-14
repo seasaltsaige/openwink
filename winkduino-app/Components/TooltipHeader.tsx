9c1863afe0b0e933491a2fda1011987532cfd362
seasaltsaige <oriontothemax@gmail.com>
2025-08-14 14:33:11 -0700

    create(tooltip): Create tooltip component

---
+++ winkduino-app/Components/TooltipHeader.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { useColorTheme } from "../hooks/useColorTheme";
+import { Pressable, Text, View } from "react-native";
+import IonIcons from "@expo/vector-icons/Ionicons";
+import Tooltip from "react-native-walkthrough-tooltip";
+
+
+interface ITooltipHeaderProps {
+  tooltipContent: React.ReactElement<any, string | React.JSXElementConstructor<any>>;
+  tooltipTitle: string;
+}
+
+export function TooltipHeader({ tooltipContent, tooltipTitle }: ITooltipHeaderProps) {
+  const { theme, colorTheme } = useColorTheme();
+  const [tooltipOpen, setTooltipOpen] = useState(false);
+
+  return (
+    <Tooltip
+      isVisible={tooltipOpen}
+      onClose={() => setTooltipOpen(false)}
+      content={tooltipContent}
+      closeOnBackgroundInteraction
+      closeOnContentInteraction
+      placement="bottom"
+      contentStyle={theme.tooltipContainer}
+    >
+
+      <View style={theme.tooltipContainerView}>
+        <Text style={theme.tooltipText}>
+          {tooltipTitle}
+        </Text>
+
+        <Pressable
+          hitSlop={20}
+          onPress={() => setTooltipOpen(true)}
+        >
+          {
+            ({ pressed }) => (
+              <IonIcons style={theme.tooltipIcon} color={pressed ? colorTheme.buttonColor : colorTheme.headerTextColor} size={24} name="help-circle-outline" />
+            )
+          }
+        </Pressable>
+      </View>
+
+
+    </Tooltip>
+  )
+}
