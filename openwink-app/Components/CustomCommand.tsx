ba66f0893785a0c118b4445953a57e17ed53cef4
seasaltsaige <oriontothemax@gmail.com>
2025-08-16 13:11:11 -0700

    continue work on custom command view

--- openwink-app/Components/CustomCommand.tsx
+++ openwink-app/Components/CustomCommand.tsx
@@ -1,12 +1,15 @@
 import { Pressable, Text, View } from "react-native";
 import { CommandOutput, CustomCommandStore } from "../Storage";
-
+import { useColorTheme } from "../hooks/useColorTheme";
+import { DefaultCommandValueEnglish } from "../helper/Constants";
 interface ICustomCommandProps {
   command: CommandOutput;
   onEdit: () => void;
 }
 
 export function CustomCommand({ command, onEdit }: ICustomCommandProps) {
+
+  const { theme, colorTheme } = useColorTheme();
 
   const onDelete = () => {
     CustomCommandStore.deleteCommand(command.name);
@@ -15,24 +18,32 @@ export function CustomCommand({ command, onEdit }: ICustomCommandProps) {
   return (
     <View
       style={{
-        backgroundColor: "pink",
+        backgroundColor: colorTheme.backgroundSecondaryColor,
         width: "80%",
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        padding: 10,
+        padding: 8,
+        paddingHorizontal: 15,
+        borderRadius: 10,
+        rowGap: 4,
+        boxShadow: "0 0px 5px rgba(0, 0, 0,0.3)"
       }}
     >
 
-      <Text>
+      <Text style={{ color: colorTheme.headerTextColor, fontSize: 18, fontFamily: "IBMPlexSans_500Medium" }}>
         {command.name}
       </Text>
 
-      <Text>
-        Command Action Body
+      <Text style={{ textAlign: "center", color: colorTheme.headerTextColor, fontSize: 16, fontFamily: "IBMPlexSans_400Regular" }}>
+        {
+          command.command ? (
+            command.command.map(cmd => cmd.transmitValue ? DefaultCommandValueEnglish[cmd.transmitValue! - 1] : `${cmd.delay} ms Delay`).join(" → ").slice(0, 50)
+          ) : "Command Data not found"
+        }...
       </Text>
 
-      <View >
+      <View style={{ display: "flex", flexDirection: "row", justifyContent: "space-evenly", alignItems: "center", width: "100%" }}>
         <Pressable onPress={onEdit}>
           <Text>Edit</Text>
         </Pressable>
