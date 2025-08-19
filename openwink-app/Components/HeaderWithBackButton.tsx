dfb76c7f877f599aefc959ae26701c99c245c8d2
seasaltsaige <oriontothemax@gmail.com>
2025-08-19 12:25:03 -0700

    add(status): device status conditional rendering

--- openwink-app/Components/HeaderWithBackButton.tsx
+++ openwink-app/Components/HeaderWithBackButton.tsx
@@ -8,9 +8,10 @@ interface IHeaderWithBackButtonProps {
   backText: string;
   headerText: string;
   headerTextStyle?: TextStyle;
+  deviceStatus?: boolean;
 }
 
-export function HeaderWithBackButton({ backText, headerText, headerTextStyle }: IHeaderWithBackButtonProps) {
+export function HeaderWithBackButton({ backText, headerText, headerTextStyle, deviceStatus }: IHeaderWithBackButtonProps) {
   const { theme, colorTheme } = useColorTheme();
   const { device, isConnecting, isScanning } = useBLE();
   const navigation = useNavigation();
@@ -30,15 +31,17 @@ export function HeaderWithBackButton({ backText, headerText, headerTextStyle }: 
                 {backText}
               </Text>
               {
-                device ? (
-                  <IonIcons style={theme.backButtonContainerIcon} name="wifi-outline" color="#367024" size={21} />
-                ) : (
-                  (isConnecting || isScanning) ?
-                    <ActivityIndicator style={theme.backButtonContainerIcon} color={colorTheme.buttonColor} />
-                    : (
-                      <IonIcons style={theme.backButtonContainerIcon} name="cloud-offline-outline" color="#b3b3b3" size={23} />
-                    )
-                )
+                deviceStatus ? (
+                  device ? (
+                    <IonIcons style={theme.backButtonContainerIcon} name="wifi-outline" color="#367024" size={21} />
+                  ) : (
+                    (isConnecting || isScanning) ?
+                      <ActivityIndicator style={theme.backButtonContainerIcon} color={colorTheme.buttonColor} />
+                      : (
+                        <IonIcons style={theme.backButtonContainerIcon} name="cloud-offline-outline" color="#b3b3b3" size={23} />
+                      )
+                  )
+                ) : <></>
               }
             </>
           )
