3a2235143e9986d54a5cc159072292c3e7a2e768
seasaltsaige <oriontothemax@gmail.com>
2025-09-22 16:18:21 -0700

    continue work on OTA update service handler

--- openwink-app/helper/Handlers/OTA.ts
+++ openwink-app/helper/Handlers/OTA.ts
@@ -1,15 +1,23 @@
+import WifiManager from "react-native-wifi-reborn";
+
+import { DeviceMACStore, FirmwareStore } from "../../Storage";
 import { UPDATE_URL } from "../Constants";
+import { generatePassword } from "../Functions";
+
+type FirmwareType = `${number}.${number}.${number}`;
 
 export abstract class OTA {
-  public static activeVersion: string = "1.0.0";
-  public static latestVersion: string = "1.0.0";
+  public static activeVersion: FirmwareType = "1.0.0";
+  public static latestVersion: FirmwareType = "1.0.0";
+  private static wifiPasskey: string;
 
-  public static async fetchUpdateVersion(authorization: string): boolean {
+  public static async fetchUpdateAvailable(): Promise<boolean> {
+    // Fetch latest software version from API using device MAC address as access code
     const response = await fetch(UPDATE_URL,
       {
         method: "GET",
         headers: {
-          authorization,
+          authorization: DeviceMACStore.getStoredMAC() ?? "",
         },
       }
     );
@@ -17,11 +25,69 @@ export abstract class OTA {
     if (!response.ok) return false;
 
     const json = await response.json();
-    const version = json["version"] as string;
+    const version = json["version"] as FirmwareType;
+    this.setLatestVersion(version);
+    this.setActiveVersion();
 
+    return this.shouldUpdate();
+  }
+
+  public static generateWifiPasskey() {
+    this.wifiPasskey = generatePassword(15);
+    return this.wifiPasskey;
+  }
+
+  public static async updateFirmware() {
+    const firmwareResponse = await fetch(`${UPDATE_URL}/firmware`,
+      {
+        method: "GET",
+        headers: {
+          authorization: DeviceMACStore.getStoredMAC() ?? "",
+        }
+      }
+    );
+
+    if (!firmwareResponse.ok) return false;
+
+    const firmwareBlob = await firmwareResponse.blob();
+    const blobWithType = firmwareBlob.slice(0, firmwareBlob.size, "application/octet-stream");
+
+    console.log("Downloaded firmware blob.");
+    console.log(blobWithType.size);
+
+    await WifiManager.connectToProtectedSSIDOnce(
+      "Wink Module: Update Access Point",
+      this.wifiPasskey,
+      false,
+      true,
+    );
+
+
+    const updateStatusResponse = await fetch("http://module-update.local/update",
+      {
+        method: "POST",
+        body: blobWithType,
+        headers: {
+          "Content-Length": blobWithType.size.toString(),
+        },
+      },
+    );
+
+    if (updateStatusResponse.ok) {
+
+    }
 
   }
 
+
+  private static setActiveVersion() {
+    const storedFirmwareVersion = FirmwareStore.getFirmwareVersion();
+    if (storedFirmwareVersion !== null) this.activeVersion = storedFirmwareVersion as FirmwareType;
+  }
+
+  private static setLatestVersion(version: FirmwareType) {
+    this.latestVersion = version;
+  }
 
   private static shouldUpdate(): boolean {
     const partsLatest = this.latestVersion.split(".");
