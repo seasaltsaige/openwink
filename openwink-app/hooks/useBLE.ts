5e78611acbc49f999982709ae96deec42d153237
James <james.west@groovnow.com>
2025-10-03 07:54:03 +1300

    refactor: remove obsolete useBLE hook

--- openwink-app/hooks/useBLE.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import { BleContext } from "../Providers/BleProvider";
-export function useBLE() {
-  const ctx = useContext(BleContext);
-  if (!ctx) {
-    throw new Error("useBLE must be inside BleProvider");
-  }
-  return ctx;
-}
