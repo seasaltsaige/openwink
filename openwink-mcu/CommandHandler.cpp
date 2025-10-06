5f077571b2bceb2ca601db67d04475cb86f03431
seasaltsaige <oriontothemax@gmail.com>
2025-10-06 13:02:05 -0700

    add: handleQueuedCustomCommand and parseCustomCommand logic to ButtonHandler

--- openwink-mcu/CommandHandler.cpp
+++ openwink-mcu/CommandHandler.cpp
@@ -1,7 +1,30 @@
+#include <string>
 #include "CommandHandler.h"
 #include "BLE.h"
 #include "BLECallbacks.h"
 #include "MainFunctions.h"
+#include "ButtonHandler.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <vector>
+
+using namespace std;
+
+vector<string> CommandHandler::commandSequence;
+
+void CommandHandler::parseCustomCommand(string command) {
+  char* command_c_str = const_cast<char*>(command.c_str());
+  char* token = strtok(command_c_str, "-");
+  while (token != NULL) {
+    commandSequence.push_back(string(token));
+    token = strtok(NULL, "-");
+  }
+
+  for (string part : commandSequence) {
+    Serial.printf("Command part: %s\n", part.c_str());
+  }
+}
 
 void CommandHandler::handleQueuedCommand() {
   BLE::setBusy(true);
@@ -60,9 +83,6 @@ void CommandHandler::handleQueuedCommand() {
       if (leftStatus != rightStatus) {
         if (leftStatus == 1) rightUp();
         else rightDown();
-
-        delay(HEADLIGHT_MOVEMENT_DELAY);
-
         setAllOff();
         BLE::updateHeadlightChars();
       }
@@ -75,9 +95,6 @@ void CommandHandler::handleQueuedCommand() {
       if (leftStatus != rightStatus) {
         if (rightStatus == 1) leftUp();
         else leftDown();
-
-        delay(HEADLIGHT_MOVEMENT_DELAY);
-
         setAllOff();
         BLE::updateHeadlightChars();
       }
@@ -86,9 +103,106 @@ void CommandHandler::handleQueuedCommand() {
 
       break;
   }
-  // delay(HEADLIGHT_MOVEMENT_DELAY);
-  setAllOff();
 
-  BLE::updateHeadlightChars();
+  setAllOff();
   BLE::setBusy(false);
+}
+
+void CommandHandler::handleQueuedCustomCommand() {
+  parseCustomCommand(queuedCustomCommand);
+  queuedCustomCommand = "";
+  BLE::setBusy(true);
+  ButtonHandler::setCustomCommandActive(true);
+
+  for (auto cmd : commandSequence) {
+    if (!ButtonHandler::customCommandActive) break;
+
+    if (cmd[0] == 'd') {
+      string portion = cmd.substr(1);
+      int time = stoi(portion);
+      delay(time);
+    } else {
+      int parsedCommand = stoi(cmd);
+      Serial.printf("Command: %d\n", parsedCommand);
+      switch (parsedCommand) {
+        // Both Up
+        case 1:
+          bothUp();
+          break;
+
+        // Both Down
+        case 2:
+          bothDown();
+          break;
+        // Both Blink
+        case 3:
+          // Should function regardless of current headlight position (ie: Left is up, right is down -> Blink Command -> Left Down Left Up AND Right Up Right Down)
+          bothBlink();
+          break;
+
+        // Left Up
+        case 4:
+          leftUp();
+          break;
+
+        // Left Down
+        case 5:
+          leftDown();
+          break;
+
+        // Left Blink (Wink)
+        case 6:
+          leftWink();
+          break;
+
+        // Right Up
+        case 7:
+          rightUp();
+          break;
+
+        // Right Down
+        case 8:
+          rightDown();
+          break;
+
+        // Right Blink (Wink)
+        case 9:
+          rightWink();
+          break;
+
+        // "Wave" left first
+        case 10:
+
+          if (leftStatus != rightStatus) {
+            if (leftStatus == 1) rightUp();
+            else rightDown();
+            setAllOff();
+            BLE::updateHeadlightChars();
+          }
+
+          leftWave();
+          break;
+
+        case 11:
+
+          if (leftStatus != rightStatus) {
+            if (rightStatus == 1) leftUp();
+            else leftDown();
+            setAllOff();
+            BLE::updateHeadlightChars();
+          }
+
+          rightWave();
+
+          break;
+      }
+
+      setAllOff();
+    }
+  }
+
+  ButtonHandler::setCustomCommandActive(false);
+  BLE::setCustomStatus(0);
+  BLE::setBusy(false);
+  commandSequence.clear();
 }
--- openwink-mcu/CommandHandler.h
+++ openwink-mcu/CommandHandler.h
@@ -1,7 +1,16 @@
 #pragma once
+#include <vector>
+#include <string.h>
+
+using namespace std;
 
 class CommandHandler {
+
 public:
+
   static void handleQueuedCommand();
+  static void handleQueuedCustomCommand();
 private:
+  static vector<string> commandSequence;
+  static void parseCustomCommand(string command);
 };
