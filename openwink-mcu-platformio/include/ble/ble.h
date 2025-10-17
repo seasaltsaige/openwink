a3d75347c74c832f0bfe6ef978dd2b09c347ac9d
seasaltsaige <oriontothemax@gmail.com>
2025-10-16 23:32:08 -0700

    begin ble init setup

--- openwink-mcu-platformio/include/ble/ble.h
+++ openwink-mcu-platformio/include/ble/ble.h
@@ -1,6 +1,30 @@
 #ifndef __BLE
 #define __BLE
 
-#include "NimBLEDevice.h"
+#include <string>
+
+using namespace std;
+
+class BLE
+{
+  public:
+    static void init(string name);
+
+    static bool getClientConnected()
+    {
+        return device_connected;
+    };
+
+    static void startDevice();
+
+  private:
+    static bool device_connected;
+    static void startServer();
+    static void startService();
+    static void startAdvertising();
+    // static void
+};
+
+void INIT_nimble_device(string name);
 
 #endif
