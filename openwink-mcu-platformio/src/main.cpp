4ee19448f00d583c10dd2f69f64e9fb8692e6328
seasaltsaige <oriontothemax@gmail.com>
2025-10-18 00:30:41 -0700

    feat: Add INIT functions

--- openwink-mcu-platformio/src/main.cpp
+++ openwink-mcu-platformio/src/main.cpp
@@ -1,3 +1,4 @@
+#include "ble/ble.h"
 #include "gpio_conf.h"
 #include "input_intr.h"
 #include "tasks_init.h"
@@ -5,7 +6,15 @@
 
 extern "C" void app_main()
 {
+    // Setup GPIO pins to receive inputs and send outputs as needed
     INIT_gpio();
+    // Setup GPIO pin interrupt service routines to handle button/motion inputs
     INIT_intr();
+    // Enable wakeup sources on startup; By default enables both GPIO wakeup AND Timer wakeup
     enable_wakeup_sources();
+
+    // Initiate and start advertising BLE server
+    INIT_nimble_device("OpenWink");
+    // Start main code functionality
+    INIT_tasks();
 }
