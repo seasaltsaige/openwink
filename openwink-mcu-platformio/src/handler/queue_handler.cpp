a104b1be0d02551e9201401430f0df2d5a40ce14
seasaltsaige <oriontothemax@gmail.com>
2025-10-18 19:43:40 -0700

    chore: Update headlight input queue to bool (true = high, false = low)

--- openwink-mcu-platformio/src/handler/queue_handler.cpp
+++ openwink-mcu-platformio/src/handler/queue_handler.cpp
@@ -3,5 +3,5 @@
 #include "freertos/FreeRTOS.h"
 
 QueueHandle_t QueueHandler::button_queue = xQueueCreate(10, sizeof(bool));
-QueueHandle_t QueueHandler::headlight_input_queue = xQueueCreate(10, sizeof(int));
+QueueHandle_t QueueHandler::headlight_input_queue = xQueueCreate(10, sizeof(bool));
 QueueHandle_t QueueHandler::headlight_output_queue = xQueueCreate(10, sizeof(int));
