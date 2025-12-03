3e2e1d64d1dcb0fa7d770095de42808a677f1a55
seasaltsaige <oriontothemax@gmail.com>
2025-12-02 22:31:50 -0800

    chore: Version number

--- openwink-mcu/constants.cpp
+++ openwink-mcu/constants.cpp
@@ -1,13 +1,15 @@
-#include "esp_attr.h"
 #include "constants.h"
+
 #include <string>
 
+#include "esp_attr.h"
 
-const std::string customButtonPressArrayDefaults[9] = { "1", "0", "0", "0", "0", "0", "0", "0", "0" };
+const std::string customButtonPressArrayDefaults[9] = { "1", "0", "0", "0", "0",
+                                                        "0", "0", "0", "0" };
 const int maxTimeBetween_msDefault = 500;
 const int sleepTime_us = 15 * 1000 * 1000;
 const int advertiseTime_ms = 750;
 int awakeTime_ms = 0;
-RTC_DATA_ATTR int HEADLIGHT_MOVEMENT_DELAY = 750;
+RTC_DATA_ATTR int HEADLIGHT_MOVEMENT_DELAY = 580;
 RTC_DATA_ATTR double leftSleepyValue = 50;
 RTC_DATA_ATTR double rightSleepyValue = 50;
