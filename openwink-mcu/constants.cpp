58775d0930b0980d47fc4cc85a6885c8db92287d
seasaltsaige <oriontothemax@gmail.com>
2025-12-04 11:47:39 -0800

    fix: Headlight delay. 580ms was too short

--- openwink-mcu/constants.cpp
+++ openwink-mcu/constants.cpp
@@ -10,6 +10,6 @@ const int maxTimeBetween_msDefault = 500;
 const int sleepTime_us = 15 * 1000 * 1000;
 const int advertiseTime_ms = 750;
 int awakeTime_ms = 0;
-RTC_DATA_ATTR int HEADLIGHT_MOVEMENT_DELAY = 580;
+RTC_DATA_ATTR int HEADLIGHT_MOVEMENT_DELAY = 600;
 RTC_DATA_ATTR double leftSleepyValue = 50;
 RTC_DATA_ATTR double rightSleepyValue = 50;
--- openwink-mcu/constants.h
+++ openwink-mcu/constants.h
@@ -15,7 +15,7 @@
 // Green wire in wiring harness, indicating busy when high
 #define OEM_HEADLIGHT_STATUS 46 // UPDATE TO 46 FOR PROD
 
-#define FIRMWARE_VERSION "0.5.4"
+#define FIRMWARE_VERSION "0.5.5"
 
 /** ---- BEGIN BLE UUID DEFINITIONS ---- **/
 // Service for headlight movements
