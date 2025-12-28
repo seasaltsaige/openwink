33df09c3ed398c9a57aa239d1a5ef8a4d776c4c4
seasaltsaige <oriontothemax@gmail.com>
2025-12-28 12:52:00 -0800

    chore: Update version numbers

--- openwink-mcu/constants.h
+++ openwink-mcu/constants.h
@@ -15,7 +15,7 @@ extern int OUT_PIN_RIGHT_UP;
 // Green wire in wiring harness, indicating busy when high
 #define OEM_HEADLIGHT_STATUS 46 // UPDATE TO 46 FOR PROD
 
-#define FIRMWARE_VERSION "0.7.7"
+#define FIRMWARE_VERSION "0.8.0"
 
 #define AUTH_TIME_MS 5000
 
