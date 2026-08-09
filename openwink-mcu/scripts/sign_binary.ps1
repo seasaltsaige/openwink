c49b7def6aae9a548b6e19e2669ab6d29b68bf8d
seasaltsaige <oriontothemax@gmail.com>
2026-08-09 15:27:00 -0700

    update: Server/bins for testing purposes

--- openwink-mcu/scripts/sign_binary.ps1
+++ openwink-mcu/scripts/sign_binary.ps1
@@ -1,1 +1,10 @@
-bin_signing --bin ./build/esp32.esp32.esp32s3/openwink-mcu.ino.bin --key private_key.pem --out ../update-server/files/update.bin --hash sha256
+param(
+    [Parameter(Mandatory = $true)]
+    [string]$Version
+)
+
+bin_signing `
+    --bin ./build/esp32.esp32.esp32s3/openwink-mcu.ino.bin `
+    --key private_key.pem `
+    --out "../update-server/files/firmware/update-$Version.bin" `
+    --hash sha256
